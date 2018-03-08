5e390f67fafa867d179a68b27fd2bb01fbe96dc8
Cameron <quiksilver980@gmail.com>
2018-03-07 17:41:36 -0800

    2nd commit

---
+++ scripts/mainpayment.js
@@ -0,0 +1,42 @@
+(function (window) {
+  "use strict";
+  var FORM_SELECTOR = "[data-payment-info=\"form\"]";
+  var TEXT_INSERT = "[data-payment-info=\"modal\"]";
+
+  //var $ = window.jQuery;
+  var App = window.App;
+
+  var FormHandler = App.FormHandler;
+  var formHandler = new FormHandler(FORM_SELECTOR);
+
+  formHandler.addSubmitHandler(function (data) {
+    console.log(data);
+    var $ = window.jQuery;
+    var title = data.title;
+    var name = data.username;
+    var $div = $("<div></div>", {
+      "id": "affirmMessage",
+      "class": "modal"
+    });
+    var $p = $("<p></p>");
+
+    var $a = $("<a></a>", {
+      "href": "#",
+      "rel": "modal:close"
+    });
+
+    var text = "Thank you for your payment," + title + name;
+
+    $p.append(text);
+    $div.append($p);
+    $div.append($a);
+    console.log($div);
+
+
+    $div.add(TEXT_INSERT);
+
+
+
+  });
+  console.log(formHandler);
+})(window);
